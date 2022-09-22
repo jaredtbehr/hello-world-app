236c64094d34d6bc4be4d38248573f854770c9e3
jaredtbehr <jtb0032@mix.wvu.edu>
2022-09-22 16:34:01 -0400

    Cards
    - Added cards
    - Touched up hero
    - Cards still need coloring for icons
    - Cards still need spacing tweaks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './navigation/top-nav-bar/top-nav-bar.component';
+import { FooterComponent } from './navigation/footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    FooterComponent,
+    HeroComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
